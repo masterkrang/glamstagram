45f668142993f54975b281c4655cf9af512c4f63
Kurt Braget (sword computer) <kurtbraget@gmail.com>
2012-05-05 15:07:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glamstagram::Application.config.secret_token = 'bc68b0dc1d72bb8b5c6810acd67c9dd8a7105ae7214a8b0243ab5ecb729086d0dc5c12337ebe04781d27afec8271480568829608a02fc2f61f7a375bba59e019'
